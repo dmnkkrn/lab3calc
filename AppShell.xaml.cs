6f46fdee180191e99185b54a60111b1babb01065
Dominik Korniluk <korniluk24153@stud.akademiabialska.pl>
2025-10-17 16:55:59 +0200

    Initialized project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace labcalcgit
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace labcalcgit
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
