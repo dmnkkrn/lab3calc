2e582dac5cc347529f1128b2a24c23bb9df0a9f9
Dominik Korniluk <korniluk24153@stud.akademiabialska.pl>
2025-10-18 15:49:38 +0200

    Added ConcentrationCalculator class

---
+++ ConcentrationCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labcalcgit
+{
+    internal class ConcentrationCalculator
+    {
+        public double CalculatePercentage(double Solute, double Solution)
+        {
+            if (Solution == 0)
+            {
+                throw new ArgumentException("Solution cannot be zero.");
+            }
+            return (Solute / Solution) * 100;
+        }
+        public double CalculateSum(double Volume, int Units)
+        {
+                        return Volume * Units;
+        }
+
+        public double CalculatePercentageWithSum(double Solute, double Volume, int Units)
+        {
+            double totalSolution = CalculateSum(Volume, Units);
+            return CalculatePercentage(Solute, totalSolution);
+        }
+
+    }
+
+
+}
